4df15a8be710da508d9c3dc8ff9f1d61fea8f874
Thomas Mey <substantialimpulse@protonmail.com>
2017-11-06 21:16:20 +0100

    rewrite logic to only sent a notification once it dropped below the threshold and only again if it was over the threshold again. Also simplify some logic and update the readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Fuel Price Notifier
-Get the current fuel price reported at the German [MTS-K system](http://www.bundeskartellamt.de/DE/Wirtschaftsbereiche/Mineral%C3%B6l/MTS-Kraftstoffe/Verbraucher/verbraucher_node.html) and sent a mail notification when the price is very low.
+Get the current fuel price reported at the German [MTS-K system](http://www.bundeskartellamt.de/DE/Wirtschaftsbereiche/Mineral%C3%B6l/MTS-Kraftstoffe/Verbraucher/verbraucher_node.html) and sent a mail notification when the price is lower than a specified percentile.
 
 This works by using the [benzinpreis-aktuell.de API](https://www.benzinpreis-aktuell.de/api2/).
 
@@ -12,24 +12,29 @@ This works by using the [benzinpreis-aktuell.de API](https://www.benzinpreis-akt
 ```python
 # -*- coding: utf-8 -*-
 
-# Required settings
-api_link_json=""
+# Required
+api_link_json = ""
 smtp_server = "smtp.gmail.com"
 email_sender = ""
 email_sender_password = ""
 email_receiver_super = ""
 email_receiver_diesel = ""
 
-# Optional settings
+# Optional
 alert_percentile = 20
 days_for_percentile = 5
 ```
 
-
 3) You have to register a new API account at https://www.benzinpreis-aktuell.de/api2/ and copy your individual JSON url into `api_link_json`
 
-4) Create a new or exisiting email account and find out the address of the SMTP server. For gmail it is prefilled, which is way I would recommend it. 
+4) Create a new email account and find out the address of the SMTP server for th email provider. For gmail it is prefilled, which is why I would recommend it.
 
-5) Enter your sender address in `email_sender` and your sender adress password (Google account password) in `email_sender_password`
+5) Enter your sender address in `email_sender` and your sender address password (i.e. Google account password) in `email_sender_password`
 
 6) Enter the email you want to receive the notification at under `email_receiver_super` or `email_receiver_diesel` or both, depending on what type of fuel you want to be notified about.
+
+
+# Optional settings
+
+* `days_for_percentile`: Number of days the `alert_percentile` is calculated from
+* `alert_percentile`: The percentile that is used to determine what a low price is. If the current price is below the value this percentile represents, a notification will be sent.
--- tanken_api.py
+++ tanken_api.py
@@ -12,51 +12,67 @@ from config import smtp_server, api_link_json, email_sender, email_sender_passwo
 # Open sqlite database
 connection = sqlite3.connect("tanken.db")
 cursor = connection.cursor()
+sqlite3.register_adapter(bool, int)
+sqlite3.register_converter("BOOLEAN", lambda v: bool(int(v)))
+
+
+class Price(object):
+    def __init__(self, super_price, diesel_price, super_sent=False, diesel_sent=False):
+        self.super_price = super_price
+        self.diesel_price = diesel_price
+        self.super_sent = super_sent
+        self.diesel_sent = diesel_sent
 
 
 def create_table():
-    sql_command = """
-    CREATE TABLE 'preis' ( 
-    time INTEGER PRIMARY KEY, 
-    price_super INTEGER, 
-    price_diesel INTEGER 
-    );"""
-    cursor.execute(sql_command)
-
-
-def insert_db(price_super, price_diesel):
-    millis = int(round(time.time() * 1000))
-    format_str = """INSERT INTO 'preis' (time, price_super, price_diesel)
-    VALUES ("{time}", "{price_super}", "{price_diesel}");"""
-    sql_command = format_str.format(time=millis, price_super=price_super, price_diesel=price_diesel)
     try:
+        sql_command = """
+        CREATE TABLE 'preis' ( 
+        time INTEGER PRIMARY KEY, 
+        price_super INTEGER, 
+        price_diesel INTEGER,
+        super_sent INTEGER DEFAULT 0,
+        diesel_sent INTEGER DEFAULT 0
+        );"""
         cursor.execute(sql_command)
-        connection.commit()
-    except Exception as e:
-        if isinstance(e, sqlite3.OperationalError) and "no such table" in format(e):
-            print("Table not created, doing it now. Next time it should work!")
-            create_table()
-            sys.exit(0)
-        else:
-            print("Error while inserting into db" + format(e))
+    except sqlite3.OperationalError:
+        pass
+
+
+def insert_into_db(price):
+    millis = int(round(time.time() * 1000))
+    format_str = """INSERT INTO 'preis' (time, price_super, price_diesel, super_sent, diesel_sent)
+    VALUES ("{time}", "{price_super}", "{price_diesel}", "{super_sent}", "{diesel_sent}");"""
+    sql_command = format_str.format(time=millis, price_super=price.super_price, price_diesel=price.diesel_price,
+                                    super_sent=(1 if price.super_sent else 0),
+                                    diesel_sent=(1 if price.diesel_sent else 0))
+    cursor.execute(sql_command)
+    connection.commit()
 
 
 def get_percentiles():
     millis = int(round(time.time() * 1000))
     # Nur die letzten 5 Tage berücksichtigen
     millis_days_ago = str(millis - (days_for_percentile * 24 * 60 * 60 * 1000))
-    cursor.execute("SELECT price_super, price_diesel FROM preis WHERE time > " + millis_days_ago)
+    cursor.execute(
+        "SELECT price_super, price_diesel, super_sent, diesel_sent FROM preis WHERE time > " + millis_days_ago +
+        " ORDER BY time DESC")
 
     super_prices = []
     diesel_prices = []
     results_db = cursor.fetchall()
+    if len(results_db) == 0:
+        return None
     for line in results_db:
-        super_prices.append(line[0])
-        diesel_prices.append(line[1])
+        super_prices.append(int(line[0]))
+        diesel_prices.append(int(line[1]))
+    last = results_db[0]
+    last_prices_db = Price(super_price=last[0], diesel_price=last[1], super_sent=bool(last[2]),
+                           diesel_sent=bool(last[3]))
     # use numpy to generate a percentile
     super_percentile_value = np.percentile(np.array(super_prices), alert_percentile)
     diesel_percentile_value = np.percentile(np.array(diesel_prices), alert_percentile)
-    return [super_percentile_value, diesel_percentile_value]
+    return [[super_percentile_value, diesel_percentile_value], last_prices_db]
 
 
 def send_mail(to, subject):
@@ -80,36 +96,49 @@ def load_current_prices():
     current_price_super = int(float(result['stations'][0]['price_super']) * 100 - 0.9)
     current_price_diesel = int(float(result['stations'][0]['price_diesel']) * 100 - 0.9)
 
-    return [current_price_super, current_price_diesel]
+    return Price(current_price_super, current_price_diesel)
 
+
+# Just try to create the table each time in case it does not exist.
+create_table()
 
 # Load current prices and store them into vars
-price_array = load_current_prices()
-price_super = price_array[0]
-price_diesel = price_array[1]
-
-# Insert the prices into the db
-insert_db(price_super, price_diesel)
+price = load_current_prices()
 
 # Load the percentiles by fetching values from db
-p_array = get_percentiles()
-p_super = p_array[0]
-p_diesel = p_array[1]
+percentile_array = get_percentiles()
+if percentile_array is None:
+    print("Using defaults as this is the first run. If not your database might be corrupt")
+    # reset to defaults as this is the first run
+    p_super = 1000
+    p_diesel = 1000
+    last_prices = Price(1000, 1000, False, False)
+else:
+    p_super = percentile_array[0][0]
+    p_diesel = percentile_array[0][1]
+    last_prices = percentile_array[1]
 
 # Debug info
-print("Super price: " + str(price_super))
+print("Super price: " + str(price.super_price))
 print("Super percentile: " + str(p_super))
 print("----------------------------")
-print("Diesel price: " + str(price_diesel))
+print("Diesel price: " + str(price.diesel_price))
 print("Diesel percentile: " + str(p_diesel))
 print("----------------------------")
 
 # Compare the super and diesel with the percentiles and sent an email if needed
-if price_super < p_super:
-    msg = "Super ist mit " + str(price_super) + "cent recht guenstig"
-    send_mail(email_receiver_super, msg)
-    print("Super notification sent")
-if price_diesel < p_diesel:
-    msg = "Diesel ist mit " + str(price_diesel) + "cent recht guenstig"
-    send_mail(email_receiver_diesel, msg)
-    print("Diesel notification sent")
+if price.super_price <= p_super:
+    price.super_sent = True
+    if not last_prices.super_sent:
+        msg = "Super ist mit " + str(price.super_price) + "cent recht guenstig"
+        send_mail(email_receiver_super, msg)
+        print("Super notification sent")
+if price.diesel_price <= p_diesel:
+    price.diesel_sent = True
+    if not last_prices.diesel_sent:
+        msg = "Diesel ist mit " + str(price.diesel_price) + "cent recht guenstig"
+        send_mail(email_receiver_diesel, msg)
+        print("Diesel notification sent")
+
+# Insert the prices into the db
+insert_into_db(price)
